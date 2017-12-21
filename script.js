fa54d16ac9d4e2700bca255d25084c2dcc085589
shlyahovdmitry <shlyahovdmitry@gmail.com>
2017-12-22 01:27:46 +0200

    all fixed, Horses done!

--- script.js
+++ script.js
@@ -1,45 +1,51 @@
 //0 TASK
-(function(){
-    function House() { 
-             this.windows = "white";
-             this.doors = "brown";
-             this.colorWindows = function (color) {
-                 this.windows = color;
-             };
-             this.colorDoors = function (color) {
-                 this.doors = color;
-             }
-             }; 
-        
+window.House = (function(){   //HOUSE Constructor
+                 function House() { 
+                    this.windows = "white";
+                    this.doors = "brown";
+                    this.colorWindows = function (color) {
+                        this.windows = color;
+                    };
+                    this.colorDoors = function (color) {
+                        this.doors = color;
+                    };
+                 };
+                 return House; 
+               })();
 
-        function Car() {
-            this.model = "a5",
-            this.brand = "audi",
-            this.mileage = "120000 km",
-            this.driver = { 
-                name: "Dmitry",
-                sername: "Shlyahov", 
-                age: "26",
-                changeSername: function (newSername){ //setfunction to change sername of driver
-                    this.sername = newSername;
-                }
-                },
-            this.go = function (distance) {
-                console.info("car drove " + distance + " km");
-                return this.mileage = parseFloat(this.mileage) + distance + " km"; //changing mileage of car
-            }
-            }
-        })();       
+window.Car = (function(){  // CAR constructor
+                function Car() {
+                    this.model = "a5";
+                    this.brand = "audi";
+                    this.mileage = "120000 km";
+                    this.driver = { 
+                        name: "Dmitry",
+                        sername: "Shlyahov", 
+                        age: "26",
+                        changeSername: function (newSername){ //setfunction to change sername of driver
+                                       this.sername = newSername;
+                                       }
+                                  },
+                    this.go = function (distance) {
+                              console.info("car drove " + distance + " km");
+                              return this.mileage = parseFloat(this.mileage) + distance + " km"; //changing mileage of car
+                              }
+                    };
+                    return Car;
+                    })();      
 
 
 //1 TASK
-(function(){
-function Rabbit (color) { //rabbit constructor
-    this.color = color;
-    this.changeColor = function (newColor) {
-        this.color = newColor;
-    }
-};
+window.Rabbit = (function(){ //RABBIT constructor
+                    function Rabbit (color) { 
+                        this.color = color;
+                    };
+
+                    Rabbit.prototype.changeColor = function (newColor) {
+                        this.color = newColor;
+                    }
+                    return Rabbit;
+                })();
 
 var whiteRabbit = new Rabbit('white');
 var greyRabbit = new Rabbit('gray');
@@ -47,26 +53,56 @@ whiteRabbit.changeColor('gold');
 greyRabbit.changeColor('gold');
 console.log(whiteRabbit, greyRabbit);
 
-function Pupil(name) { //pupil constructor
-    this.name = name;
-    this.evaluation = [];
-    this.evaluate = function (evaluate) {
-        this.evaluation.push(evaluate);
-    }
-}
-    
+window.Pupil = (function () {
+                    function Pupil(name) { //pupil constructor
+                        this.name = name;
+                        this.evaluation = [];
+                    };
 
-var summMileages = 0;
-function Horse(name) { //horse constructor
-    this.name = name;
-    this.mileage = '100';
-    this.summMileages = summMileages;
-    this.go = function (distance) {
-                this.mileage = parseFloat(this.mileage) + distance + ' km';
-                summMileages += distance;
-              };
-    summMileages += parseFloat(this.mileage); 
-};
+                    Pupil.prototype.evaluate =  function (evaluate) {
+                        this.evaluation.push(evaluate);
+                    };
+                    return Pupil;
+                })();
 
-})();
 
+window.Horse = (function (){    //HORSE constructor
+                    function Horse(name){
+                        this.name = name;
+                        this.mileage = 0;
+                        this.fatigue = 0;
+                        this.count = 0;
+                    };
+
+                    Horse.prototype = {
+                        constructor: Horse,
+                        allMileage: 0,
+                        run: function go (distance) {
+                                if(this.count < distance) {
+                                    if(this.fatigue !== 10) {
+                                        this.fatigue++;
+                                        this.count++;
+                                        this.run (distance); 
+                                    }
+                                    if(this.fatigue == 10) {
+                                        this.fatigue = 0;
+                                        var ConcreteHorse = this;
+                                        setTimeout(function(){
+                                            console.log('Horse resting 1 second');
+                                            ConcreteHorse.run (distance);
+                                        }, 1000)
+                                    }
+                                    if(this.count === distance){
+                                        this.count = 0;
+                                        this.mileage += distance;
+                                        Horse.prototype.allMileage += distance;
+                                   }
+                                }
+                              
+                            }   
+                    }
+                    return Horse;
+                })();
+//////////////////////////////////////
+                var a = new Horse(); // testing Horses
+                a.run(33);
